a8f4c9d270dac6ef27c70133cd12dd583a52485b
Marcus Kleppe <marcuskleppe@gmail.com>
2016-08-01 19:26:31 +0200

    add basic CRUD methods

--- imports/features/SomeFeature/methods.js
+++ imports/features/SomeFeature/methods.js
@@ -13,6 +13,44 @@ export const createGroup = new ValidatedMethod({
   run({ name }) {
     console.log('Executing on client?', this.isSimulation);
     console.log('Got name:', name);
-    return name;
+    const id = Groups.insert({
+      name,
+    });
+    return id;
+  },
+});
+
+export const updateGroup = new ValidatedMethod({
+  name: 'updateGroup',
+
+  validate(args) {
+    check(args, {
+      _id: String,
+      name: String,
+    });
+  },
+
+  run({ _id, name }) {
+    console.log('Executing on client?', this.isSimulation);
+    console.log('Got name:', name);
+    Groups.update(_id, {
+      name,
+    });
+    return true;
+  },
+});
+
+export const deleteGroup = new ValidatedMethod({
+  name: 'deleteGroup',
+
+  validate(args) {
+    check(args, String); // the ID
+  },
+
+  run(id) {
+    console.log('Executing on client?', this.isSimulation);
+    console.log('Got id:', id);
+    Groups.remove(id);
+    return true;
   },
 });
