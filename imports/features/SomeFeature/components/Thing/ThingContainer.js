c1ee1c7cfbafa529c14ac24a76b43d4bb216621a
Marcus Kleppe <marcuskleppe@gmail.com>
2016-08-01 19:44:48 +0200

    add sub

--- imports/features/SomeFeature/components/Thing/ThingContainer.js
+++ imports/features/SomeFeature/components/Thing/ThingContainer.js
@@ -1,15 +1,19 @@
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import { createGroup as _createGroup } from '../../methods';
 
 import Thing from './Thing';
 export default createContainer(() => {
+  const groupsHandle = Meteor.subscribe('groups.all');
+
   // Maybe write a higher-order function for this later
   const createGroup = (args, callback) => {
     _createGroup.call(args, callback);
   };
 
   return {
-    meteorData: 'goes here',
+    ready: groupsHandle.ready(),
+    groups: Groups.find({}).fetch(),
     createGroup,
   };
 }, Thing);
