fb7dd9fd817476ada7f2402f83bbeac38e46ad11
Marcus Kleppe <marcuskleppe@gmail.com>
2017-01-19 13:45:20 +0100

    helper function to get opponent data

--- imports/features/SomeFeature/components/table/table.js
+++ imports/features/SomeFeature/components/table/table.js
@@ -34,6 +34,32 @@ class Table extends Component {
     }
   }
 
+  getOpponentPlayer = () => {
+    const game = this.props.games[0];
+    if (game.player1Id === this.props.user._id) {
+      // Current user is player1, player2 is opponent.
+      return {
+        _id: game.player2Id,
+        balance: game.player2Balance,
+        name: game.player2Name,
+      };
+    }
+    // Current user is player2 which means that player1 is the opponent.
+    return {
+      _id: game.player1Id,
+      balance: game.player1Balance,
+      name: game.player1Name,
+    };
+  }
+
+  isOpponentLive = () => {
+    const opponent = this.getOpponentPlayer();
+    if (!opponent._id) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
     return (
       <div>
@@ -42,7 +68,7 @@ class Table extends Component {
           <h2>Table</h2>
           <h3>You: {this.props.user.username}</h3>
           <p>Your stack: {this.getUserStack()}</p>
-          {this.isOpponentLive
+          {this.isOpponentLive()
             ? <div>
                 <h3>Opponent: {this.getOpponentName()}</h3>
               </div>
